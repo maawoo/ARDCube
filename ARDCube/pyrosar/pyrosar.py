3a1f18e6298ae4358e79aca6c25c31ce5506161c
maawoo <56583917+maawoo@users.noreply.github.com>
2021-01-29 15:13:37 +0100

    Minor changes

--- ARDCube/pyrosar/pyrosar.py
+++ ARDCube/pyrosar/pyrosar.py
@@ -3,9 +3,9 @@ from pyroSAR import snap
 from spatialist.ancillary import finder
 
 maindir = '/home/marco/pypypy/00_data/pyro_test'
-outdir = os.path.join(maindir, 'out')
+outdir = os.path.join(maindir, 'out_2')
 snap_gpt = '/opt/snap/bin/gpt'
-shape = os.path.join(maindir, 'misc', 'jena.geojson')  # TH_shape_25832.gpkg
+shape = os.path.join(maindir, 'misc', 'th_stripe.geojson')  # TH_shape_25832.gpkg
 dem_lidar = os.path.join(maindir, 'dem', 'TH_LidarDEM_10m.tif')  # 10m
 dem_srtm = os.path.join(maindir, 'dem', 'TH_SRTM_30m.tif')  # 30m
 epsg = 25832
@@ -17,7 +17,7 @@ for scene in scenes:
     print(os.path.basename(scene))
 
     snap.geocode(infile=scene, outdir=outdir, t_srs=epsg, tr=20,
-                 shapefile=shape, scaling='db', allow_RES_OSV=True,
+                 shapefile=None, scaling='db', allow_RES_OSV=True,
                  externalDEMFile=dem_srtm, externalDEMApplyEGM=False, groupsize=1,
                  export_extra=None, test=False
                  )
--- ARDCube/pyrosar/sentinelsat.py
+++ ARDCube/pyrosar/sentinelsat.py
@@ -4,24 +4,33 @@ from datetime import date
 import geojson
 import logging
 
-logging.basicConfig(filename='test.log', filemode='a', format='%(message)s', level='INFO')
-# logging.basicConfig(format='%(message)s', level='INFO')
+# logging.basicConfig(filename='sentinelsat.log', filemode='a', format='%(message)s', level='INFO')  # save to file
+# logging.basicConfig(format='%(message)s', level='INFO')  # print in console
+
+############################################################
 
 maindir = '/home/marco/pypypy/00_data/pyro_test'
 s1_dir = os.path.join(maindir, 'S1')
-aoi = os.path.join(maindir, 'misc', 'jena.geojson')
+s2_dir = os.path.join(maindir, 'S2')
+aoi = os.path.join(maindir, 'misc', 'th_stripe.geojson')
+timespan = ('20200601', '20200606')
+s1_producttype = 'GRD'
+s2_cloudcover = (0, 80)  # min, max (percentage)
+
+#############################################################
 
 api = SentinelAPI('maawoo', '2ZSuBPsU8YQkzUsDz6c3pS8nMn', 'https://scihub.copernicus.eu/apihub/')
 footprint = geojson_to_wkt(read_geojson(aoi))
-products = api.query(footprint,
-                     date=('20200601', '20200701'),
-                     platformname='Sentinel-1',
-                     producttype='GRD')
 
-print(f"Total size of queried products: {api.get_products_size(products)} GB")
+products_s1 = api.query(footprint, date=timespan, platformname='Sentinel-1', producttype=s1_producttype)
+products_s2 = api.query(footprint, date=timespan, platformname='Sentinel-2', cloudcoverpercentage=s2_cloudcover)
+print(f"Sentinel-1: {len(products_s1)} scenes // {api.get_products_size(products_s1)} GB total file size \n"
+      f"Sentinel-2: {len(products_s2)} scenes // {api.get_products_size(products_s2)} GB total file size")
 
 # api.download(id=list(products.keys())[0])  # Download single product based on index
-api.download_all(products, directory_path=s1_dir)  # Download all queried products
+api.download_all(products_s1, directory_path=s1_dir)  # Download all queried products (s1)
+# api.download_all(products_s2, directory_path=s2_dir)  # Download all queried products (s2)
+
 
 """
 ## Export footprints and metadata of queried products as GeoJSON
