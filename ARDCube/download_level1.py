231780c7e135e79af68a12cf256f2cc8db2fe441
maawoo <56583917+maawoo@users.noreply.github.com>
2021-03-15 16:49:14 +0100

    Create function for S1 download

--- ARDCube/download_level1.py
+++ ARDCube/download_level1.py
@@ -1,56 +1,63 @@
-from ARDCube import read_settings
+from ARDCube.read_settings import get_settings
 
 import os
+import logging
+from datetime import datetime
 from sentinelsat import SentinelAPI, read_geojson, geojson_to_wkt
 from spython.main import Client
-from datetime import date
-import geojson
-import logging
-
-logging.basicConfig(filename='sentinelsat.log', filemode='a', format='%(message)s', level='INFO')  # save to file
-logging.basicConfig(format='%(message)s', level='INFO')  # print in console
-settings = read_settings.get_settings('GENERAL')
-
-####################
-
-maindir = '/home/marco/pypypy/pyro_test'
-s1_dir = os.path.join(maindir, 'S1')
-
-####################
-
-aoi = settings['']
-aoi = os.path.join(maindir, 'misc', 'th_stripe.geojson')
-timespan = ('20200601', '20200604')
-s1_producttype = 'GRD'
-
-#############################################################
-
-api = SentinelAPI('maawoo', '2ZSuBPsU8YQkzUsDz6c3pS8nMn', 'https://scihub.copernicus.eu/apihub/')
-footprint = geojson_to_wkt(read_geojson(aoi))
-
-products_s1 = api.query(footprint, date=timespan, platformname='Sentinel-1', producttype=s1_producttype)
-products_s2 = api.query(footprint, date=timespan, platformname='Sentinel-2', producttype=s2_producttype,  cloudcoverpercentage=s2_cloudcover)
-print(f"Sentinel-1: {len(products_s1)} scenes // {api.get_products_size(products_s1)} GB total file size \n"
-      f"Sentinel-2: {len(products_s2)} scenes // {api.get_products_size(products_s2)} GB total file size")
-
-# api.download(id=list(products.keys())[0])  # Download single product based on index
-api.download_all(products_s1, directory_path=s1_dir)  # Download all queried products (s1)
-# api.download_all(products_s2, directory_path=s2_dir)  # Download all queried products (s2)
 
 
-"""
-## Export footprints and metadata of queried products as GeoJSON
-
-all_path = os.path.join(maindir, 'misc', 'test.geojson')
-all_geojson = api.to_geojson(products)
-with open(all_path, 'w') as f:
-    geojson.dump(all_geojson, f)
-"""
-
-def force_download(settings):
-    """
-    force-level1-csd -s sensors -d daterange -c cloudcover metadata_dir level-1_dir queue.txt aoi-file
-    :return:
+def download_s1(log=False):
+    """Download Sentinel-1 GRD data from Copernicus Open Access Hub based on parameters defined in 'settings.prm'.
+    Incomplete downloads are continued and complete files are skipped.
     """
 
-    force_c = Client.instance(os.path.join(maindir, 'singularity/force', 'force.sif'), name='force_c')
+    ## Get user defined settings
+    settings = get_settings()
+
+    ## Define output directory
+    main_dir = settings.get('GENERAL', 'DataDirectory')
+    s1_dir = os.path.join(main_dir, 'level-1/S1')
+    if not os.path.exists(s1_dir):
+        os.makedirs(s1_dir)
+
+    ## https://sentinelsat.readthedocs.io/en/stable/api.html#logging
+    if log:
+        filename = os.path.join(main_dir, 'log', f"{datetime.now().strftime('%Y-%m-%d-%H-%M-%S__download_s1')}.log")
+        logging.basicConfig(filename=filename, filemode='w', format='%(message)s', level='INFO')
+
+    ## Get footprint from AOI
+    if os.path.isdir(settings.get('GENERAL', 'AOI')):  # Returns True if a path was provided
+        aoi_path = settings.get('GENERAL', 'AOI')
+        footprint = geojson_to_wkt(read_geojson(aoi_path))
+    else:
+        aoi_path = os.path.join(main_dir, 'misc/aoi', settings.get('GENERAL', 'AOI'))
+        footprint = geojson_to_wkt(read_geojson(aoi_path))
+
+    ## Get timespan
+    timespan = (settings.get('GENERAL', 'TimespanMin'),
+                settings.get('GENERAL', 'TimespanMax'))
+
+    ## Connect to Copernicus Open Access Hub using provided credentials.
+    ## The API defaults to https://scihub.copernicus.eu/apihub
+    api = SentinelAPI(settings.get('GENERAL', 'CopernicusUser'),
+                      settings.get('GENERAL', 'CopernicusPassword'))
+
+    ## Perform a query using provided parameters
+    query = api.query(footprint,
+                      date=timespan,
+                      platformname='Sentinel-1',
+                      producttype='GRD')
+
+    print(f"{len(query)} scenes // {api.get_products_size(query)} GB total file size")
+
+    api.download_all(query, directory_path=s1_dir)  # Download all queried products (s1)
+
+
+#def force_download():
+#    """
+#    force-level1-csd -s sensors -d daterange -c cloudcover metadata_dir level-1_dir queue.txt aoi-file
+#    :return:
+#    """
+
+#    force_c = Client.instance(os.path.join(maindir, 'singularity/force', 'force.sif'), name='force_c')
