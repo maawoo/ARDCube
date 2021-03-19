5fd6a15d307e4f7a5ac3a35d4d3147e4a92522da
maawoo <56583917+maawoo@users.noreply.github.com>
2021-03-19 16:42:52 +0100

    Add SAT_DICT

--- ARDCube/config.py
+++ ARDCube/config.py
@@ -4,3 +4,12 @@ ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 FORCE_PATH = os.path.join(ROOT_DIR, 'singularity/force', 'force.sif')
 PYROSAR_PATH = os.path.join(ROOT_DIR, 'singularity/pyrosar', 'pyrosar.sif')
 POSTGRES_PATH = os.path.join(ROOT_DIR, 'singularity/postgres', 'postgres.sif')
+
+## Key = Satellite sensor name as listed in settings.prm
+## Value = Abbreviation used in FORCE download module
+SAT_DICT = {'Sentinel1': None,
+            'Sentinel2': 'S2A,S2B',
+            'Landsat4': 'LT04',
+            'Landsat5': 'LT05',
+            'Landsat7': 'LE07',
+            'Landsat8': 'LC08'}
