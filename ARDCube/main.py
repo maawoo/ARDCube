66357d6b72123b2e7c6d276d8a42b8cc26545933
maawoo <56583917+maawoo@users.noreply.github.com>
2021-03-25 14:38:03 +0100

    Add main.py to use via command line

---
+++ ARDCube/main.py
@@ -0,0 +1,25 @@
+from ARDCube.download_level1 import download_level1
+from ARDCube.generate_ard import generate_ard
+
+import sys
+
+
+def main():
+    # Optional params?
+    # Print help when only 'help' is given as command?
+
+    command = sys.argv[1]
+    sensor = sys.argv[2]
+
+    if command == 'download':
+        download_level1(sensor=sensor,
+                        debug_force=False)
+    elif command == 'generate':
+        generate_ard(sensor=sensor,
+                     debug_force=False)
+    else:
+        raise ValueError(f"Command '{command}' not recognized.")
+
+
+if __name__ == '__main__':
+    main()
