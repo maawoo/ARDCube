2e82031d4f5337fbc66ad684d984315ba1cfe67a
maawoo <56583917+maawoo@users.noreply.github.com>
2021-03-25 12:30:44 +0100

    More changes related to last commit. (summary in body)
    Summary of major changes included with this and the last ( a little chaotic) commit:
    - Remove list of sensors from settings.prm, which was only used by the download script; add sensor parameter to download_level1() instead
    - Move get_settings() to utils.py and delete check function
    - Move create_dem() to utils.py and add get_dem_path(), which calls create_dem() if necessary
    - General minor changes in output and terminology

--- ARDCube/download_level1.py
+++ ARDCube/download_level1.py
@@ -1,6 +1,5 @@
-from ARDCube.config import FORCE_PATH
-from ARDCube.settings import get_settings
-from ARDCube.utils import get_aoi_path, check_sat_settings
+from ARDCube.config import FORCE_PATH, SAT_DICT
+from ARDCube.utils import get_settings, get_aoi_path
 
 import os
 import logging
@@ -9,44 +8,43 @@ from sentinelsat import SentinelAPI, read_geojson, geojson_to_wkt
 from spython.main import Client
 
 
-def download_level1(debug_force=False):
+def download_level1(sensor, debug_force=False):
     """Main download script."""
+
+    ## Check if sensor is supported.
+    if sensor not in list(SAT_DICT.keys()):
+        raise NotImplemented(f"{sensor} is not supported!")
 
     ## Get user defined settings
     settings = get_settings()
 
-    ## Check which satellite sensors were selected (= True) in the settings.prm file and create dictionary with
-    ## a few useful values (abbreviation used in FORCE, directory to store downloaded scenes, ...)
-    sats = check_sat_settings(settings=settings)
-
     ## Start download functions
     ## Both functions print query information first and then ask for confirmation to start the download.
-    for sat in list(sats.keys()):
-        print(f"#### \nStarting download script for {sat}...")
+    print(f"#### Start downloading {sensor} data...")
 
-        if sat == 'Sentinel1':
-            download_sar(settings=settings,
-                         out_dir=sats[sat]['level1_dir'])
-        else:
-            download_optical(settings=settings,
-                             out_dir=sats[sat]['level1_dir'],
-                             force_abbr=sats[sat]['force_abbr'],
-                             debug_force=debug_force)
+    if sensor == 'Sentinel1':
+        download_sar(settings=settings)
+
+    else:
+        download_optical(settings=settings,
+                         sensor=sensor,
+                         debug_force=debug_force)
 
 
-def download_sar(settings, out_dir):
+def download_sar(settings):
     """Download Sentinel-1 GRD data from Copernicus Open Access Hub based on parameters defined in 'settings.prm'.
     https://github.com/sentinelsat/sentinelsat
     """
     ## TODO: Include SAROrbitDirection to query only ascending/descending scenes
 
-    ## Log output by default
+    out_dir = os.path.join(settings['GENERAL']['DataDirectory'], 'level1', 'Sentinel1')
+
+    ## Create logfile for sentinelsat output by default
     ## https://sentinelsat.readthedocs.io/en/stable/api.html#logging
     log_file = os.path.join(settings['GENERAL']['DataDirectory'], 'log',
                             f"{datetime.now().strftime('%Y%m%dT%H%M%S__download_sentinel1')}.log")
     if not os.path.exists(os.path.dirname(log_file)):
         os.makedirs(os.path.dirname(log_file))
-
     logging.basicConfig(filename=log_file, filemode='w', format='%(message)s', level='INFO')
 
     ## Get footprint from AOI
@@ -54,19 +52,21 @@ def download_sar(settings, out_dir):
     footprint = geojson_to_wkt(read_geojson(aoi_path))
 
     ## Get timespan
-    timespan = (settings['DOWNLOAD']['TimespanMin'], settings['DOWNLOAD']['TimespanMax'])
+    timespan = (settings['DOWNLOAD']['TimespanMin'],
+                settings['DOWNLOAD']['TimespanMax'])
 
     ## Connect to Copernicus Open Access Hub using provided credentials.
     ## The API defaults to https://scihub.copernicus.eu/apihub
-    api = SentinelAPI(settings['DOWNLOAD']['CopernicusUser'], settings['DOWNLOAD']['CopernicusPassword'])
+    api = SentinelAPI(settings['DOWNLOAD']['CopernicusUser'],
+                      settings['DOWNLOAD']['CopernicusPassword'])
 
     ## Perform a query using provided parameters
-    query = api.query(footprint,
+    query = api.query(area=footprint,
                       date=timespan,
                       platformname='Sentinel-1',
                       producttype='GRD')
 
-    ## Before starting the download, ask for user confirmation. (and print out query information)
+    ## Before starting the download, print out query information and then ask for user confirmation.
     while True:
         answer = input(f"{len(query)} Sentinel-1 GRD files were found between {timespan[0]} and {timespan[1]} \n"
                        f"for the AOI defined by '{aoi_path}'. \n"
@@ -75,36 +75,40 @@ def download_sar(settings, out_dir):
                        f"Do you want to proceed with the download? (y/n)")
 
         if answer in ['y', 'yes']:
+            print("#### Starting download...")
             api.download_all(query, directory_path=out_dir)
             break
 
         elif answer in ['n', 'no']:
+            print("#### Download cancelled...")
             break
 
         else:
-            print(f"---------- \n{answer} is not a valid answer! \n----------")
+            print(f"{answer} is not a valid answer!")
             continue
 
 
-def download_optical(settings, out_dir, force_abbr, debug_force=False):
+def download_optical(settings, sensor, debug_force=False):
     """Download optical satellite data from Google Cloud Storage based on parameters defined in 'settings.prm'.
     https://force-eo.readthedocs.io/en/latest/howto/level1-csd.html#tut-l1csd
     """
 
     ## TODO: Check if metadata catalogues exist and if not download them first!
 
+    Client.debug = debug_force
+
     ## Collect all information that will be used in the query
-    sensors = force_abbr
+    force_abbr = SAT_DICT[sensor]
     daterange = f"{settings['DOWNLOAD']['TimespanMin']}," \
                 f"{settings['DOWNLOAD']['TimespanMax']}"
     cloudcover = f"{settings['DOWNLOAD']['OpticalCloudCoverRangeMin']}," \
                  f"{settings['DOWNLOAD']['OpticalCloudCoverRangeMax']}"
-    meta_dir = os.path.join(settings['GENERAL']['DataDirectory'], 'meta/force')
+    meta_dir = os.path.join(settings['GENERAL']['DataDirectory'], 'meta/catalogues')
+    out_dir = os.path.join(settings['GENERAL']['DataDirectory'], 'level1', sensor)
     aoi_path = get_aoi_path(settings)
 
     ## Send query to FORCE Singularity container as dry run first ("--no-act") and print output
-    Client.debug = debug_force
-    output = Client.execute(FORCE_PATH, ["force-level1-csd", "--no-act", "-s", sensors, "-d", daterange,
+    output = Client.execute(FORCE_PATH, ["force-level1-csd", "--no-act", "-s", force_abbr, "-d", daterange,
                                          "-c", cloudcover, meta_dir, out_dir, "queue.txt", aoi_path],
                             options=["--cleanenv"])
 
@@ -122,23 +126,23 @@ def download_optical(settings, out_dir, force_abbr, debug_force=False):
 
         if answer in ['y', 'yes']:
 
-            print("\n#### \nStarting download... \n"
+            print("#### Starting download... \n"
                   "Depending on the dataset size and your internet speed, this might take a while. \n"
                   "Unfortunately there's currently no good solution to show the download progress. \n"
                   "If the download takes longer than you intended, you can just cancel the process \n"
                   "and start it again at a later time using the same settings in the settings.prm file. \n"
                   "FORCE automatically checks for existing scenes and will only download new scenes!")
 
-            Client.execute(FORCE_PATH, ["force-level1-csd", "-s", sensors, "-d", daterange,
+            Client.execute(FORCE_PATH, ["force-level1-csd", "-s", force_abbr, "-d", daterange,
                                         "-c", cloudcover, meta_dir, out_dir, "queue.txt", aoi_path],
                            options=["--cleanenv"])
 
             break
 
         elif answer in ['n', 'no']:
-            print("\n#### \nDownload cancelled...")
+            print("#### Download cancelled...")
             break
 
         else:
-            print(f"---------- \n{answer} is not a valid answer! \n----------")
+            print(f"{answer} is not a valid answer!")
             continue
