2b0693b387cef81bfd1c4f7c28253293fa59972c
maawoo <56583917+maawoo@users.noreply.github.com>
2021-03-26 10:48:14 +0100

    Add FORCE version number and bump current container to v3.6.5

--- ARDCube/config.py
+++ ARDCube/config.py
@@ -1,7 +1,7 @@
 import os
 
 ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-FORCE_PATH = os.path.join(ROOT_DIR, 'singularity/force', 'force.sif')
+FORCE_PATH = os.path.join(ROOT_DIR, 'singularity/force', 'force_365.sif')
 PYROSAR_PATH = os.path.join(ROOT_DIR, 'singularity/pyrosar', 'pyrosar.sif')
 POSTGRES_PATH = os.path.join(ROOT_DIR, 'singularity/postgres', 'postgres.sif')
 
