814a7ae983114d9bda3496e4b094d97c7d03c4be
maawoo <56583917+maawoo@users.noreply.github.com>
2021-04-06 13:29:55 +0200

    Adding progress bar that doesn't need additional dependencies

--- ARDCube/utils.py
+++ ARDCube/utils.py
@@ -2,6 +2,7 @@ from ARDCube.config import ROOT_DIR, PYROSAR_PATH
 
 import configparser
 import os
+import sys
 
 
 def get_settings():
@@ -130,3 +131,38 @@ def gpkg_to_geojson(gpkg_path):
                     dst.write(rec)
 
     return out_file
+
+
+def progress(count, total, status=''):
+    """
+    Source: https://gist.github.com/vladignatyev/06860ec2040cb497f0f3
+
+    # The MIT License (MIT)
+    # Copyright (c) 2016 Vladimir Ignatev
+    #
+    # Permission is hereby granted, free of charge, to any person obtaining
+    # a copy of this software and associated documentation files (the "Software"),
+    # to deal in the Software without restriction, including without limitation
+    # the rights to use, copy, modify, merge, publish, distribute, sublicense,
+    # and/or sell copies of the Software, and to permit persons to whom the Software
+    # is furnished to do so, subject to the following conditions:
+    #
+    # The above copyright notice and this permission notice shall be included
+    # in all copies or substantial portions of the Software.
+    #
+    # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+    # INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+    # PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+    # FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT
+    # OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+    # OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+    """
+
+    bar_len = 60
+    filled_len = int(round(bar_len * count / float(total)))
+
+    percents = round(100.0 * count / float(total), 1)
+    bar = '=' * filled_len + '-' * (bar_len - filled_len)
+
+    sys.stdout.write('[%s] %s%s ...%s\r' % (bar, percents, '%', status))
+    sys.stdout.flush()
