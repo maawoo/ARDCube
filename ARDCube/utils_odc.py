561fc69a716a90dc888eece5d5b450e8bd252992
maawoo <56583917+maawoo@users.noreply.github.com>
2021-04-15 15:39:46 +0200

    Add utils_odc.py

---
+++ ARDCube/utils_odc.py
@@ -0,0 +1,20 @@
+from ARDCube.config import POSTGRES_PATH
+
+import os
+from spython.main import Client
+
+
+def odc_db_init():
+    pass
+
+
+def odc_db_check():
+    pass
+
+
+def odc_index_products():
+    pass
+
+
+def odc_index_datasets():
+    pass
