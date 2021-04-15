8a47ba4766d2993864e2591c608807499425a4b1
maawoo <56583917+maawoo@users.noreply.github.com>
2021-04-15 15:40:45 +0200

    Add functions to start/stop postgres container

--- ARDCube/utils_odc.py
+++ ARDCube/utils_odc.py
@@ -4,6 +4,62 @@ import os
 from spython.main import Client
 
 
+def start_postgres(port=5433, debug=False):
+
+    ## https://www.postgresql.org/docs/10/app-pg-ctl.html
+    ## https://hub.docker.com/r/postgis/postgis
+
+    Client.debug = debug
+    if debug:
+        quiet = False
+    else:
+        quiet = True
+
+    pg_dir = os.path.dirname(POSTGRES_PATH)
+    pg_log = os.path.join(pg_dir, 'postgres_log')
+    pg_data = os.path.join(pg_dir, 'postgres_data')
+    pg_run = os.path.join(pg_dir, 'postgres_run')
+
+    output = Client.run(POSTGRES_PATH, ["pg_ctl", "start", f"--log={pg_log}", f"--options='-p {port}'", "--silent"],
+                        bind=[f"{pg_data}:/var/lib/postgresql/data", f"{pg_run}:/var/run/postgresql"],
+                        quiet=quiet)
+
+    if output is None:
+        print(f"PostgreSQL server started successfully! Port: {port}")
+    else:
+        if isinstance(output, list):
+            for line in output:
+                print(line)
+        else:
+            print(output)
+
+
+def stop_postgres(debug=False):
+
+    Client.debug = debug
+    if debug:
+        quiet = False
+    else:
+        quiet = True
+
+    pg_dir = os.path.dirname(POSTGRES_PATH)
+    pg_data = os.path.join(pg_dir, 'postgres_data')
+    pg_run = os.path.join(pg_dir, 'postgres_run')
+
+    output = Client.run(POSTGRES_PATH, ["pg_ctl", "stop", "--silent"],
+                        bind=[f"{pg_data}:/var/lib/postgresql/data", f"{pg_run}:/var/run/postgresql"],
+                        quiet=quiet)
+
+    if output is None:
+        print("PostgreSQL server stopped successfully!")
+    else:
+        if isinstance(output, list):
+            for line in output:
+                print(line)
+        else:
+            print(output)
+
+
 def odc_db_init():
     pass
 
