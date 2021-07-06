f4c2cbeff2c0ec46778cf9982ffdf1d82191e10e
maawoo <56583917+maawoo@users.noreply.github.com>
2021-07-06 22:07:56 +0200

    Minor changes

--- ARDCube/cli.py
+++ ARDCube/cli.py
@@ -14,37 +14,42 @@ def cli():
 
 
 @cli.command()
-@click.option('-d', '--dir', required=True, type=click.Path(writable=True, readable=True))
-def setup(directory):
+@click.option('-p', '--path', required=True, type=click.Path(writable=True, readable=True),
+              help='Path to a (preferably) empty directory that you want to use for your project. The necessary '
+                   'directory structure is set up and some files (e.g., for parameterization) are copied over to get '
+                   'things started.')
+def setup(path):
     click.echo('Setting up project directory.')
 
-    if not os.path.isdir(directory):
-        os.mkdir(directory)
+    if not os.path.isdir(path):
+        os.mkdir(path)
 
-    setup_project(directory=directory)
+    setup_project(directory=path)
 
 
 @cli.command()
 @click.option('-s', '--sensor', required=True, type=click.Choice(list(SAT_DICT.keys()), case_sensitive=True))
-@click.option('--debug', is_flag=True, help='Print debugging information of the FORCE Singularity container. '
-                                            'Has no effect when downloading SAR data, as no Singularity container is '
-                                            'being used.')
+@click.option('--debug', is_flag=True,
+              help='Print debugging information of the FORCE Singularity container. Has no effect when downloading SAR '
+                   'data, as no Singularity container is being used.')
 def download(sensor, debug):
     download_level1(sensor=sensor, debug=debug)
 
 
 @cli.command()
 @click.option('-s', '--sensor', required=True, type=click.Choice(list(SAT_DICT.keys()), case_sensitive=True))
-@click.option('--debug', is_flag=True, help='Print debugging information of the Singularity container used during '
-                                            'processing.')
-@click.option('--clean', is_flag=True, help='Automatically remove intermediate processing results that are created '
-                                            'during processing of SAR data. Has no effect when processing optical data.')
+@click.option('--debug', is_flag=True,
+              help='Print debugging information of the Singularity container used during processing.')
+@click.option('--clean', is_flag=True,
+              help='Automatically remove intermediate processing results that are created during processing of SAR '
+                   'data. Has no effect when processing optical data.')
 def process(sensor, debug, clean):
     generate_ard(sensor=sensor, debug=debug, clean=clean)
 
 
 @cli.command()
 @click.option('-s', '--sensor', required=True, type=click.Choice(list(SAT_DICT.keys()), case_sensitive=True))
-@click.option('-o', '--overwrite', default=True, help='If set to False, only YAML files for new scenes will be created.')
+@click.option('-o', '--overwrite', default=True,
+              help='If set to False, only YAML files for new scenes will be created.')
 def prepare(sensor, overwrite):
     prepare_odc(sensor=sensor, overwrite=overwrite)
