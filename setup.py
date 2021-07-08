6fa2c5cfee2e073b61be26e45b393387231f01f0
maawoo <56583917+maawoo@users.noreply.github.com>
2021-07-08 21:18:45 +0200

    Several changes regarding installation

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
-from io import open
+import pathlib
 
-with open("README.md", encoding="utf-8") as f:
-    long_description = f.read()
+here = pathlib.Path(__file__).parent.resolve()
+long_description = (here / 'README.md').read_text(encoding='utf-8')
 
 setup(
     name='ARDCube',
@@ -10,19 +10,18 @@ setup(
     description="Utility to create Analysis Ready (Earth Observation) Data Cubes",
     long_description=long_description,
     long_description_content_type='text/markdown',
+    url="https://github.com/maawoo/ARDCube",
+    author="Marco Wolsza",
+    author_email="marco.wolsza@uni-jena.de",
+    license='MIT',
     classifiers=[
         'License :: OSI Approved :: MIT License',
         'Operating System :: POSIX :: Linux',
-        'Programming Language :: Python'],
-    packages=find_packages(),
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3 :: Only'],
+    packages=find_packages(where='.'),
     include_package_data=True,
-    setup_requires=['setuptools_scm'],
-    use_scm_version=True,
     install_requires=open("requirements.txt").read().splitlines(),
-    author="Marco Wolsza",
-    author_email="marco.wolsza@uni-jena.de",
-    url="https://github.com/maawoo/ARDCube",
-    license='MIT',
     zip_safe=False,
     entry_points={
         'console_scripts': ['ardcube=ARDCube.cli:cli']
